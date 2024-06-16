5401b80a7767f190168cc79e75b4e5193a4c6d60
rcm0208 <c_m__00@outlook.jp>
2024-06-16 23:54:16 +0900

    add: optimismを追加、abiがない際の挙動を調整

--- frontend/app/mint/components/mint-top.tsx
+++ frontend/app/mint/components/mint-top.tsx
@@ -1,13 +1,16 @@
 import Section from '@/app/home/components/section';
 import NetworkCardlist from '@/components/network-cardlist';
 import { networkConfig } from '@/config/network-config';
+import { standardERC721AbiMap } from '@/config/standard-erc721-abi-map';
 
 export default function MintTop() {
   return (
     <Section title="Mint" subTitle="NFT Minting of ERC721">
       <div className="grid lg:grid-cols-3 gap-4">
         {networkConfig
-          .filter((network) => network.mintCollectionAddress)
+          .filter(
+            (network) => network.mintCollectionAddress && standardERC721AbiMap[network.networkId]
+          )
           .map((network) => (
             <NetworkCardlist key={network.networkId} network={network} />
           ))}
