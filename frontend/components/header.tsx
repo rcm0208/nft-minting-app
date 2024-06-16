031971b28eaba6db0220a06caf98fe5be0ffe61c
rcm0208 <c_m__00@outlook.jp>
2024-06-17 02:01:02 +0900

    add: web3-modalをライトモードとダークモードに対応

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,17 +1,32 @@
 'use client';
 
 import Link from 'next/link';
+import { useEffect } from 'react';
 import { Button } from './ui/button';
 import { ModeToggle } from './mode-toggle';
+import { useTheme } from 'next-themes';
+import { useWeb3ModalTheme } from '@web3modal/ethers/react';
+import Image from 'next/image';
 
 export default function Header() {
   const navItems = [{ label: 'Mint', href: '/mint' }];
+  const { theme } = useTheme();
+  const { setThemeMode } = useWeb3ModalTheme();
+
+  useEffect(() => {
+    if (theme) {
+      const mode = theme === 'system' ? 'light' : (theme as 'light' | 'dark');
+      setThemeMode(mode);
+    }
+  }, [theme, setThemeMode]);
 
   return (
     <header className="border-b">
       <div className="h-16 container flex items-center justify-between">
         <h1 className="font-bold">
-          <Link href="/">LOGO</Link>
+          <Link href="/">
+            <Image src="/image/logo.svg" alt="logo" width={40} height={40}></Image>
+          </Link>
         </h1>
         <div className="flex items-center gap-4">
           <ul className="flex gap-4">
