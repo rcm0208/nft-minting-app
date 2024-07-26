332348d4dfffdaf92292b86b0a02e6a927a58357
rcm0208 <c_m__00@outlook.jp>
2024-07-26 23:40:29 +0900

    add: メタデータの設定

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getURL = () => {
+  const url = process.env.VERCEL_PROJECT_PRODUCTION_URL;
+
+  return url ? `https://${url}` : `http://localhost:${process.env.PORT || 3000}`;
+};
