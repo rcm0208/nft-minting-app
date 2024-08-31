e4c7a131a156c3a5ef07d5f6848cb621a5265468
rcm0208 <c_m__00@outlook.jp>
2024-08-31 23:22:20 +0900

    fix: github actionsでこけていたのでリレーヤーがない場合は0アドレスを使うようにした

--- contract/ignition/modules/gasless-erc721a/deploy.ts
+++ contract/ignition/modules/gasless-erc721a/deploy.ts
@@ -3,40 +3,41 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-// リレーヤーアドレスの存在を確認
-if (!process.env.RELAYER_ADDRESS) {
-  throw new Error("RELAYER_ADDRESS must be set in the environment variables");
-}
+// 0アドレスの定義
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+// リレーヤーアドレスの設定（環境変数がない場合は0アドレスを使用）
+const RELAYER_ADDRESS = process.env.RELAYER_ADDRESS || ZERO_ADDRESS;
 
 export const GASLESS_ERC721A_PARAMS = {
-  CONTRACT_NAME: "Gasless ERC721A", // コントラクトの名前
-  CONTRACT_SYMBOL: "GLERC721A", // コントラクトのシンボル
-  INITIAL_BASE_URI: "", // 初期基本URI
-  MAX_SUPPLY: 30, // 発行上限
-  MAX_MINT_PER_ADDRESS: 3, // アドレスごとの発行上限
-  RELAYER_ADDRESS: process.env.RELAYER_ADDRESS as string, //リレーヤーアドレス
+	CONTRACT_NAME: "Gasless ERC721A", // コントラクトの名前
+	CONTRACT_SYMBOL: "GLERC721A", // コントラクトのシンボル
+	INITIAL_BASE_URI: "", // 初期基本URI
+	MAX_SUPPLY: 30, // 発行上限
+	MAX_MINT_PER_ADDRESS: 3, // アドレスごとの発行上限
+	RELAYER_ADDRESS: RELAYER_ADDRESS, // リレーヤーアドレス
 };
 
 const GaslessERC721AModule = buildModule("GaslessERC721AModule", (m) => {
-  const {
-    CONTRACT_NAME,
-    CONTRACT_SYMBOL,
-    INITIAL_BASE_URI,
-    MAX_SUPPLY,
-    MAX_MINT_PER_ADDRESS,
-    RELAYER_ADDRESS,
-  } = GASLESS_ERC721A_PARAMS;
+	const {
+		CONTRACT_NAME,
+		CONTRACT_SYMBOL,
+		INITIAL_BASE_URI,
+		MAX_SUPPLY,
+		MAX_MINT_PER_ADDRESS,
+		RELAYER_ADDRESS,
+	} = GASLESS_ERC721A_PARAMS;
 
-  const GaslessERC721A = m.contract("GaslessERC721A", [
-    CONTRACT_NAME,
-    CONTRACT_SYMBOL,
-    INITIAL_BASE_URI,
-    MAX_SUPPLY,
-    MAX_MINT_PER_ADDRESS,
-    RELAYER_ADDRESS,
-  ]);
+	const GaslessERC721A = m.contract("GaslessERC721A", [
+		CONTRACT_NAME,
+		CONTRACT_SYMBOL,
+		INITIAL_BASE_URI,
+		MAX_SUPPLY,
+		MAX_MINT_PER_ADDRESS,
+		RELAYER_ADDRESS,
+	]);
 
-  return { GaslessERC721A };
+	return { GaslessERC721A };
 });
 
 export default GaslessERC721AModule;
