1a7d2623e36e86a2e0901ea99fb61e8376b29d20
rcm0208 <c_m__00@outlook.jp>
2024-09-03 02:08:58 +0900

    fix: ファイル名変更

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  // basePath: '/nft-minting-app',
+	output: "standalone",
+	// basePath: '/nft-minting-app',
 };
 
 module.exports = nextConfig;
